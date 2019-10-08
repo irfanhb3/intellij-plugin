fe724a807ac75b73a5902e9a092b26897aec848c
Irfan Hussain Bhati <irfan.bhati@byjus.com>
2019-10-08 21:40:21 +0530

    simplest plugin created

---
+++ src/main/java/MyAction.java
@@ -0,0 +1,20 @@
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationType;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class MyAction extends AnAction{
+
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        NotificationGroup noti = new NotificationGroup("myplugin", NotificationDisplayType.BALLOON, true);
+        noti.createNotification("My Title",
+                "My Message",
+                NotificationType.INFORMATION,
+                null
+        ).notify(e.getProject());
+    }
+}
